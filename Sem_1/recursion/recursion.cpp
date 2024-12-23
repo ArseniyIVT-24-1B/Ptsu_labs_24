9e2dfb8c025a5a1a67f4a346bb7758ba3599f48c
Arseniy Konovalov <arseniykonovalov3001@gmail.com>
2024-12-23 10:16:09 +0500

    рекурсия

---
+++ Sem_1/recursion/recursion.cpp
@@ -0,0 +1,23 @@
+﻿//Вариант 9. Посчитать НОД для двух натуральных чисел.
+#include<iostream>
+#include<cmath>
+using namespace std;
+int dof(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+
+	return dof(b,a % b);
+
+}
+
+int main()
+{
+	int num1, num2,result;
+	cin >> num1 >> num2;
+	result = dof(num1, num2);
+
+	cout << result;
+
+	return 0;
+}
