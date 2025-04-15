7115fcf530fdf910b4606a12b24c35beee88d925
Arseniy Konovalov <arseniykonovalov3001@gmail.com>
2025-04-15 11:11:36 +0500

    Классы

---
+++ Sem_2/Clases/2/Computer.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include "Computer.h"
+
+Computer::Computer()
+{
+	cpu = "";
+	ram = 0;
+	hdd = 0;
+}
+Computer::Computer(std::string a, int b, int c)
+{
+	cpu = a;
+	ram = b;
+	hdd = c;
+	std::cout <<  this << std::endl;
+}
+Computer::Computer(const Computer& a)
+{
+	cpu = a.cpu;
+	ram = a.ram;
+	hdd = a.hdd;
+}
+Computer::~Computer()
+{
+	std::cout << this << std::endl;
+}
+std::string Computer::get_cpu()
+{
+	return cpu;
+}
+int Computer::get_ram()
+{
+	return ram;
+}
+int Computer::get_hdd()
+{
+	return hdd;
+}
+void Computer::set_cpu(std::string N)
+{
+	cpu = N;
+}
+void Computer::set_ram(int N)
+{
+	ram = N;
+}
+void Computer::set_hdd(int N)
+{
+	hdd = N;
+}void Computer::show()
+{
+	std::cout << cpu << ram << hdd;
+}
---
+++ Sem_2/Clases/5/Vector.h
@@ -0,0 +1,16 @@
+﻿﻿#pragma once
+#include "Person.h"
+
+class Vector {
+private:
+    int size;
+    int current;
+    Person** data;
+public:
+    Vector();
+    Vector(int size);
+    ~Vector();
+
+    void add(Person* person);
+    friend std::ostream& operator<<(std::ostream& out, const Vector& v);
+};
